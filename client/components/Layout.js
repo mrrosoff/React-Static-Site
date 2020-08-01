e7228776e4998de043836480c1cec69d4dd1b90a
Max <maxrosoff1@gmail.com>
2020-08-01 13:06:05 -0600

    Update Packages and Fix Scroll Issue

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Button, Grid, Typography } from "@material-ui/core";
+import { Button, Container, Grid, Typography } from "@material-ui/core";
 import {sendServerRequest} from "../utils/api";
 
 const Layout = () =>
@@ -8,22 +8,24 @@ const Layout = () =>
 	let request = {requestType: "postRequest", payload: "Test"};
 
 	return (
-		<Grid
-			container
-			direction={"column"}
-			justify={"center"}
-			alignContent={"center"}
-			alignItems={"center"}
-			spacing={2}
-			style={{height: "100vh"}}
+		<Container>
+			<Grid
+				container
+				direction={"column"}
+				justify={"center"}
+				alignContent={"center"}
+				alignItems={"center"}
+				spacing={2}
+				style={{height: "100vh"}}
 			>
-			<Grid item>
-				<Typography variant={"h2"}>Hello World!</Typography>
+				<Grid item>
+					<Typography variant={"h2"}>Hello World!</Typography>
+				</Grid>
+				<Grid item>
+					<Button color={"primary"} onClick={() => sendServerRequest(request)}>Click Me To Hit API</Button>
+				</Grid>
 			</Grid>
-			<Grid item>
-				<Button color={"primary"} onClick={() => sendServerRequest(request)}>Click Me To Hit API</Button>
-			</Grid>
-		</Grid>
+		</Container>
 	)
 };
 
