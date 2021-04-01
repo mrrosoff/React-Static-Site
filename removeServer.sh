17b993a42da7cfc628792a5992f903e7295f472e
Max Rosoff <me@maxrosoff.com>
2021-04-01 11:56:11 -0700

    oh yeah delete this bad line

--- removeServer.sh
+++ removeServer.sh
@@ -4,6 +4,4 @@ rm -rf ./client/utils/api.js ./server ./node_modules
 npm r axios concurrently express lodash module-alias morgan nodemon
 npm install
 
-sed 'import {sendServerRequest}' ./client/components/Layout.js
-
 rm -rf ./removeServer.sh
